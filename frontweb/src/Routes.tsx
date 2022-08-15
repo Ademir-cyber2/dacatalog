8533b9dd20b676eb81bccfae7045d8c2edb3dade
Ademir Aparecido Pereira da Silva <adm2.silva@yahoo.com>
2022-08-15 15:05:48 -0300

    ProductDetails route

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -5,6 +5,7 @@ import { Switch } from "react-router-dom";
 import Navbar from "components/Navbar";
 import Catalog from "pages/Catalog";
 import Admin from "pages/Admin";
+import ProductDetails from "pages/ProductDetails";
 
 
 const Routes = () => (
@@ -14,8 +15,11 @@ const Routes = () => (
             <Route path="/" exact>
                 <Home />
             </Route>
-            <Route path="/products">
+            <Route path="/products" exact>
                 <Catalog />
+            </Route>
+            <Route path="/products/:productId">
+                <ProductDetails />
             </Route>
             <Route path="/admin">
                 <Admin />
