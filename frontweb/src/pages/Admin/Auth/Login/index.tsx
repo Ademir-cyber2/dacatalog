ad06324b12e8d64809bce9ae9b87cf77133fb429
Ademir Aparecido Pereira da Silva <adm2.silva@yahoo.com>
2022-12-20 22:25:41 -0300

    Form validation
    Form validation

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -16,7 +16,7 @@ const Login = () => {
 
     const [hasError, setHasError] = useState(false);
 
-    const { register, handleSubmit } = useForm<FormData>();
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
     const onSubmit = (formData: FormData) => {
         requestBackendLogin(formData)
@@ -42,21 +42,29 @@ const Login = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
                     <input
-                        {...register("username")}
+                        {...register("username", {
+                            required: 'Campo obrigatorio',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: 'Email invalido'
+                            }
+                        })}
                         type="text"
                         className="form-control base-input"
                         placeholder="Email"
                         name="username"
                     />
+                    <div className='invalid-feedback d-block'>{errors.username?.message}</div>
                 </div>
                 <div className="mb-2">
                     <input
-                        {...register("password")}
+                        {...register("password", { required: 'Campo obrigatorio' })}
                         type="password"
                         className="form-control base-input "
                         placeholder="Password"
                         name="password"
                     />
+                    <div className='invalid-feedback d-block'>{errors.password?.message}</div>
                 </div>
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha
