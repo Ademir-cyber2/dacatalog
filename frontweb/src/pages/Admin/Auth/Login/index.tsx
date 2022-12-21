208c18906eb56eef2efd6ac44d5c78b1b5417663
Ademir Aparecido Pereira da Silva <adm2.silva@yahoo.com>
2022-12-20 21:21:36 -0300

    Manual error
    Manual error login

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import './styles.css';
 import { type } from '@testing-library/user-event/dist/type';
 import { requestBackendLogin } from 'util/requests';
+import { useState } from 'react';
 
 type FormData = {
     username: string;
@@ -13,14 +14,18 @@ type FormData = {
 
 const Login = () => {
 
+    const [hasError, setHasError] = useState(false);
+
     const { register, handleSubmit } = useForm<FormData>();
 
     const onSubmit = (formData: FormData) => {
         requestBackendLogin(formData)
             .then(response => {
+                setHasError(false);
                 console.log('SUCESSO', response);
             })
             .catch(error => {
+                setHasError(true);
                 console.log('ERRO', error);
             }
             )
@@ -29,6 +34,11 @@ const Login = () => {
     return (
         <div className="base-card login-card">
             <h1>LOGIN</h1>
+            {hasError && (
+                <div className='alert alert-danger'>
+                    Erro ao tentar efetuar o login
+                </div>
+            )}
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
                     <input
